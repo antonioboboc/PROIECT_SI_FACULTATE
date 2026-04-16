e3b482b60c5680daf252b309d31f253325d930b1
BizeraRazvan <bizera.razvan.w5w@student.ucv.ro>
2026-04-16 17:47:18 +0300

    first logic implement

--- src/main.c
+++ src/main.c
@@ -1,28 +1,53 @@
 #include "drivers/gpio/gpio.h"
+#include "drivers/servo/servo.h"
 #include "drivers/ultrasonic-sensor/ultrasonic.h"
 #include "utils/delay.h"
 #include "bsp/nano.h"
+#include <avr/interrupt.h>
 #include <stdint.h>
 
-#define LED_DISTANCE_THRESHOLD_CM 20U
+
 
 int main(void) {
-    uint16_t distance_cm = 0;
 
+    uint16_t distance_cm = 0;
+    uint8_t radar_state = 0;
+    Timer0_Init();
+    Servo_Init();
     ultrasonic_Init();
-    GPIO_Init(LED_BUILTIN, GPIO_OUTPUT);
-    GPIO_Write(LED_BUILTIN, GPIO_LOW);
+    Buzzer_Init();
+    sei();
+
+
 
     while (1) {
-        distance_cm = ultrasonic_Read();
 
-        if ((distance_cm > 0U) && (distance_cm < LED_DISTANCE_THRESHOLD_CM)) {
-            GPIO_Write(LED_BUILTIN, GPIO_HIGH);
-        } else {
-            GPIO_Write(LED_BUILTIN, GPIO_LOW);
+        / Sweep 0° → 180° in 1° steps /
+          for (uint8_t angle = 0; angle <= 180; angle++) {
+            Servo_SetAngle(angle);
+            Delay(15);   / ~15 ms between steps for smooth motion /
         }
 
-        Delay(50);
+        / Sweep 180° → 0° in 1° steps/
+        for (int16_t angle = 180; angle >= 0; angle--) {
+            Servo_SetAngle((uint8_t)angle);
+            Delay(15);
+        }
+
+
+
+
+        distance_cm = ultrasonic_Read();
+
+
+        radar_state = Radar_return_state(distance_cm);
+        if(radar_state == RADAR_STATE_WARNING) {
+            Radar_WarningStateAction();
+        } 
+        else {
+            Radar_SafeStateAction();
+        }
+        Delay(100);
     }
 
     return 0;
